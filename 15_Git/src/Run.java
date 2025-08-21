bbeff93c69646d1e79d9d50e890e679baf39f95b
wookidoki <wookidoki98@gamil.com>
2025-08-21 11:12:05 +0900

    Create Haha Class

--- 15_Git/src/Run.java
+++ 15_Git/src/Run.java
@@ -3,7 +3,10 @@ public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// git add .
+		// git commit -m
+		// git push origin master
+		// 오늘도 열심히 공부했다
 	}
 
 }
